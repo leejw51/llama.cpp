251b5a574245b05ddb290e6411397f2a9ba6ed88
jongwhan <leejw51@gmail.com>
2023-11-04 19:37:29 +0900

    auto-command
    ok
    
    ok
    
    add command
    
    ignore

---
+++ go2.sh
@@ -0,0 +1,3 @@
+#python3 convert.py models/7B/
+#./build/bin/quantize ./models/7B/ggml-model-f16.gguf ./models/7B/ggml-model-q4_0.gguf q4_0
+./build/bin/main -m ./models/7B/ggml-model-q4_0.gguf -n 128 -p "what is rust lang?"
