9941a75b9de129526e536a08529d237890bee630
jongwhan <leejw51@gmail.com>
2024-01-07 22:27:49 +0900

    data check

--- finetuning/data.py
+++ finetuning/data.py
@@ -2,14 +2,16 @@ from datasets import load_dataset
 
 #dataset_name = "mlabonne/guanaco-llama2-1k"
 #dataset_name= "elliotthwang/guanaco-llama2-chinese-1k"
-dataset_name="baohuynhbk14/vietnamese-guanaco-llama2-1k"
-dataset=load_dataset(dataset_name)
+#dataset_name="baohuynhbk14/vietnamese-guanaco-llama2-1k"
+dataset_name= "maywell/korean_textbooks"
+dataset=load_dataset(dataset_name,"tiny-textbooks")
+dataset['train'] = dataset['train'].select(range(1000))
 print(dataset)
 
-# First entry in the dataset
-first_entry = dataset['train'][0]
-print("First Entry:", first_entry)
 
-# Last entry in the dataset
-last_entry = dataset['train'][-1]
-print("Last Entry:", last_entry)
+
+# Displaying the first entry of the dataset
+print("First entry:", dataset['train'][0])
+
+# Displaying the last entry of the dataset
+print("Last entry:", dataset['train'][-1])
